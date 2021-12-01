92856ddd4153fe28c9d349787d2ca8eeff5eaa20
yanntrc <yann.troadec.5@gmail.com>
2021-12-01 10:56:58 +0100

    last commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react'
+import { ethers } from 'ethers'
+import Token from './artifacts/contracts/Token.sol/Token.json'
+
 import './App.css';
 
+const tokenAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3'
+
 function App() {
+
+  const [balance, setBalance] = useState()
+
+  useEffect(() => {
+    getBalance()
+  })
+
+  async function getBalance() {
+    if (typeof window.ethereum !== 'undefined') {
+      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      const contract = new ethers.Contract(tokenAddress, Token.abi, provider)
+      const balance = await contract.balanceOf(accounts[0])
+      setBalance(balance / (10 ** 18))
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <p>Vous avez {balance} TRC</p>
+    </div>)
 }
 
-export default App;
+export default App
